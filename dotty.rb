8feabd422254962ca2b8ef64fa3d0a2d2fe5f940
Magnolia.K <magnolia.k@icloud.com>
2020-11-09 06:09:07 +0900

    とりあえず修正

--- dotty.rb
+++ dotty.rb
@@ -1,9 +1,10 @@
 class Dotty < Formula
-  desc "Experimental Scala Compiler"
+  desc "A next-generation compiler for Scala"
   homepage "http://dotty.epfl.ch/"
   url "https://github.com/lampepfl/dotty/releases/download/3.0.0-M1/scala3-3.0.0-M1.tar.gz"
   sha256 "3e92c7f72779a645af61fdd63da3f4073518ed3471e26aec553a6d858c1387d1"
   # mirror "https://www.scala-lang.org/files/archive/scala-2.12.2.tgz"
+  version_scheme 1
 
   bottle :unneeded
 
@@ -25,8 +26,8 @@ class Dotty < Formula
       }
     EOS
 
-    shell_output("#{bin}/dotc #{file}")
-    out = shell_output("#{bin}/dotr Test").strip
+    shell_output("#{bin}/scalac #{file}")
+    out = shell_output("#{bin}/scala Test").strip
 
     assert_equal "4", out
 
@@ -37,7 +38,7 @@ class Dotty < Formula
     Hello, world!
     =============
     EOS
-    shell_output("#{bin}/dotd -siteroot #{testpath}/site -project Hello #{file}")
+    shell_output("#{bin}/scalad -siteroot #{testpath}/site -project Hello #{file}")
     index_file = File.open("#{testpath}/site/_site/index.html", "rb").read
     assert index_file.include? '<h1><a href="#hello-world" id="hello-world">Hello, world!</a></h1>'
   end
